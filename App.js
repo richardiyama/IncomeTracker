964602ac96d315f878bf13f9dc5f6ef69b2ce5e3
Richard Iyama <richardiyama@yahoo.com>
2020-11-06 16:09:50 +0100

    Added Barchart implementation

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import React, { useState } from 'react';
-import { StyleSheet,View,Text,SafeAreaView, TextInput, Button,ScrollView} from 'react-native';
+import React, { useState,useEffect } from 'react';
+import { StyleSheet,Dimensions,View,Text,SafeAreaView, TextInput, Button,ScrollView} from 'react-native';
 import Todo from './Todo';
+import {
+  BarChart,
+  LineChart
+} from "react-native-chart-kit";
 
 const App = () => {
 
   const[ description, setDescription] =  useState('')
-  const[amount, setAmount] =  useState(0)
+  const[amount, setAmount] =  useState('')
+  const[total, setTotal] = useState(0)
   const[gigs,setGigs] = useState([
     {
       description: 'Freelance Job with Richie',
       amount:500.00
     }
-  ])
+  ]);
 
-  const addTodo = () =>{
-    setTodos([input,...todos]);
-    setInput('');
+  useEffect(() => {
+    setTotal(gigs.reduce((total,gig) =>total + Number(gig.amount), 0));
+   
+  }, [gigs])
+
+  const addGig = () =>{
+    setGigs([...gigs,{
+      description: description,
+      amount: amount
+    }]);
+    setDescription('');
+    setAmount('');
 
   }
     return (
@@ -23,31 +37,88 @@ const App = () => {
         <View>
           <Text style={styles.titleText}>Let's build a React Native App for Freelance Devs to Track Income 🚀🚀🚀 </Text>
         </View>
-        
-        
+        <View>
+  <Text>Bezier Line Chart</Text>
+  <LineChart
+    data={{
+      labels: ["January", "February", "March", "April", "May", "June"],
+      datasets: [
+        {
+          data: [
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100
+          ]
+        }
+      ]
+    }}
+    width={Dimensions.get("window").width} // from react-native
+    height={220}
+    yAxisLabel="$"
+    yAxisSuffix="k"
+    yAxisInterval={1} // optional, defaults to 1
+    chartConfig={{
+      backgroundColor: "#e26a00",
+      backgroundGradientFrom: "#fb8c00",
+      backgroundGradientTo: "#ffa726",
+      decimalPlaces: 2, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      style: {
+        borderRadius: 16
+      },
+      propsForDots: {
+        r: "6",
+        strokeWidth: "2",
+        stroke: "#ffa726"
+      }
+    }}
+    bezier
+    style={{
+      marginVertical: 8,
+      borderRadius: 16
+    }}
+  />
+</View>
+        <Text>Total Income: ${total}</Text>
         <TextInput
-        style={styles.todoInput}
+        style={styles.input}
         value={description}
         placeholder='Enter a description'
         onChangeText ={text => setDescription(text)}
         />
 
         <TextInput
-        style={styles.todoInput}
+        style={styles.input}
         value={amount}
+        placeholder='Enter the amount you made in USD ($)'
         keyboardType='numeric'
         onChangeText ={text => setAmount(text)}
         />
-        {/*<Button title='Add TODO' onPress={addTodo}/>*/}
+        <Button disabled={!amount && !description} title='Add Gig 🚀' onPress={addGig}/>
+
+        {gigs.map(gig =>(
+          <View>
+               <Text>{gig.description}</Text>
+               <Text>${gig.amount}</Text>
+               </View>
+
+        ))}
+
+
       </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
 
-  todoInput:{
+  input:{
 height: 40,
-margin: 20,
+marginTop: 10,
+padding:20,
 borderColor: 'red',
 borderWidth: 1
   },
